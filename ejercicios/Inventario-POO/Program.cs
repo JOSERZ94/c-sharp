ed90b2bb2015b51827d1366269e5054977ff1c5f
Jose Ramirez <jsramirez@unah.hn>
2020-10-25 19:40:54 -0600

    inventario finalizado

--- ejercicios/Inventario-POO/Program.cs
+++ ejercicios/Inventario-POO/Program.cs
@@ -18,6 +18,8 @@ namespace inventario
                 Console.WriteLine("1 - Productos");
                 Console.WriteLine("2 - Ingreso de Inventario");
                 Console.WriteLine("3 - Salida de Inventario");
+                Console.WriteLine("4 - Ajuste negativo de Inventario");
+                Console.WriteLine("5 - Ajuste positivo de Inventario");
                 Console.WriteLine("0 - Salir");
                 opcion = Console.ReadLine();
 
@@ -28,7 +30,16 @@ namespace inventario
                         break;
                     case "2":
                         inventario.ingresoDeInventario();
-                        break;                        
+                        break;  
+                    case "3":
+                        inventario.salidaDeInventario();
+                        break;    
+                    case "4":
+                        inventario.ajustenegativoDeInventario(); 
+                        break;  
+                     case "5":
+                        inventario.ajustepositivoDeInventario(); 
+                        break;                         
                     default:
                     break;
                 }
